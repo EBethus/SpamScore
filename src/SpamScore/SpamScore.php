6f6d38cbe1a51c1f6e6973ddb7bb9bb50d4c49c5
Alberto <alberroteran@gmail.com>
2020-10-30 18:02:28 -0300

    Fixed score

--- src/SpamScore/SpamScore.php
+++ src/SpamScore/SpamScore.php
@@ -10,19 +10,30 @@ class SpamScore
 
     public function score($text)
     {
-        $text = preg_replace('/[\d=|#@$\-]/mi', '', $text);
-        $text = preg_replace('/\s{2,}/mi', '', $text);
+        $text = preg_replace('/[=“‴|#@$\.\+\-]/mi', '', $text);
         $text = preg_replace('/\v/mi', '', $text);
+        $text = preg_replace('/\s{2,}/mi', '', $text);
         $keywords = json_decode(file_get_contents(resource_path('spam.json')));
         $master = strtolower(strip_tags(html_entity_decode($text, ENT_QUOTES | ENT_XML1, 'UTF-8')));
+        //echo $master;
+        preg_match_all('/\d{4,}/i', $master, $numbers, PREG_SET_ORDER);
+
+        $matchs = 0;
+        $words = 0;
+        $total_number = 0;
+        if ($numbers && $numbers[0]) {
+            $matchs = count($numbers[0]) * 10;
+            $total_number = count($numbers[0]);
+        }
+
         $total = 0;
         $counter = [];
-        $words = str_word_count($master);
-        $matchs = 0;
+        $words = max(0, str_word_count($master)-$total_number);
+
         foreach ($keywords as $keyword => $w) {
             $count = substr_count($master, $keyword);
             $matchs += ($count * str_word_count($keyword));
-            $total += ($w ** $count - 1);
+            $total += ($w ** $count)-1;
             if ($count > 1) {
                 $counter[$keyword] = $count;
             }
